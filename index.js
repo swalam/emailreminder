a85695e0bfdb6c61e1c277177a9ae808e465664a
Sayed Alam <76666550+swalam@users.noreply.github.com>
2024-08-19 21:12:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 env.config();
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-  });
-  db.connect();
+// const db = new pg.Client({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT,
+//   });
+//   db.connect();
 
 app.use(express.static("public"));
 
